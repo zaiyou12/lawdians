1e36b1debaaa9550a9c2e68210e57fbef4479203
Aaron <zaiyou12@gmail.com>
2017-06-09 03:40:37 +0900

    Readd user main pages

--- app/main/views.py
+++ app/main/views.py
@@ -6,3 +6,28 @@ from . import main
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/hospital')
+def hospital():
+    return render_template('hospital.html')
+
+
+@main.route('/lawyer')
+def lawyer():
+    return render_template('lawyer.html')
+
+
+@main.route('/event')
+def event():
+    return render_template('event.html')
+
+
+@main.route('/post-script')
+def post_script():
+    return render_template('post_script.html')
+
+
+@main.route('/contact')
+def contact():
+    return render_template('contact.html')
